20ab606126129c1907a5f202ad8b59968b1e71f8
bcrescitelli <brandoncrescitelli@yahoo.com>
2025-11-30 19:40:31 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://themartiniproject.com https://*.squarespace.com https://*.squarespace-cdn.com;",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
